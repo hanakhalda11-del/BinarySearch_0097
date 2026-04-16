0b0175388c7170dd5109cae9160711d408cd5346
hanakhalda11-del <hanakhalda11@gmail.com>
2026-04-16 18:03:04 +0700

    Implement input function for array size validation
    Added input function to prompt user for array size.

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -5,5 +5,19 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
 
-}
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. silahkan coba lagi.\n";
+        }
+    }
